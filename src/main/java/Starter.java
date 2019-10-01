9cb4c0e151367903ae42048a047bc675636107e2
Laveesh Toshniwal <laveesh.t@tcs.com>
2019-10-01 15:11:57 +0300

    add more comments

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -10,14 +10,19 @@ public class Starter {
         // (11,17,21),50
         // (2,3,5),5
         Scanner scanner = new Scanner(System.in);
+        //load input line from system in
         String input = scanner.nextLine();
+        //initialize a optimization task
         TankerOptimizationTask tankerOptimizationTask = new TankerOptimizationTask(input);
+        //provide optimizationtask to engine
         EfficientDeliveryOptimizerEngine engine = new EfficientDeliveryOptimizerEngine(tankerOptimizationTask);
+        //if oil not fully distributed to tankers,
+        // print -> minimum oil required
+        //else
+        //print efficient solutions found
         if (engine.getMinOilLeft() > 0) {
-            // min oil we need to add
             System.out.println(engine.getMinOilLeft());
         } else {
-            //print to console
             engine.getEfficientSolutionsList().forEach(es -> System.out.println(es));
         }
     }
--- src/main/java/domain/EfficientDeliveryOptimizerEngine.java
+++ src/main/java/domain/EfficientDeliveryOptimizerEngine.java
@@ -15,22 +15,26 @@ public class EfficientDeliveryOptimizerEngine {
 
     private int find(List<Integer> tankers, int barrels, String accumulator, int minOilLeft, List<String> efficientSolutions) {
         int tankerCapacity = tankers.get(0);
-        int noOfTankers = barrels / tankerCapacity;
+        int noOfTankers = barrels / tankerCapacity; //find max instance of tanker capacity which could be used
         int oilLeft = barrels % tankerCapacity;
 
-        if (tankers.size() == 1 || (tankerCapacity > barrels && barrels != 0)) {
-            if (oilLeft == 0) {
+        if (tankers.size() == 1 || (tankerCapacity > barrels && barrels != 0)) { //optimize: if no oil left return minOil
+            if (oilLeft == 0) { //tankers.size() == 1 && oilLeft == 0 , efficient solution condition
                 String efficientString = accumulator.substring(1) + "," + noOfTankers;
                 efficientSolutions.add(efficientString);
-                minOilLeft = oilLeft;
-            } else if (oilLeft < minOilLeft) {
+                minOilLeft = 0; //set minOil to 0 once any efficient solution is found
+            } else if (oilLeft < minOilLeft) { //keep updating minimumOilLeft after each combination tanker capacity index
                 minOilLeft = oilLeft;
             }
             return minOilLeft;
         }
 
+        //For each tanker capacity, decement loop till noOfTankers >= 0
+        //for each tanker capacity index, recurse on remaining tanker capacities to check if any combination satisfies the total oil amount
         while (noOfTankers >= 0) {
-            String sol = accumulator + "," + noOfTankers;
+            String sol = accumulator + "," + noOfTankers; //accumulator for current tanker capacity multipliers
+            //optimize: pass sublist excluding the current tankerCapacity
+            //efficientSolutions -> accumulator for all solutions found
             minOilLeft = find(tankers.subList(1, tankers.size()), barrels - (noOfTankers * tankerCapacity), sol, minOilLeft, efficientSolutions);
             noOfTankers--;
         }
